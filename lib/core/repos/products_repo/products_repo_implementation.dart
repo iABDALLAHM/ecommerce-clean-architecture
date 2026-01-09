ca5d7925585c891bcbfa81871fa9386c37ffe42e
Abdallah Mohamed <115114629+AMOHAME0@users.noreply.github.com>
2026-01-09 11:00:47 +0200

    implement getProducts

---
+++ lib/core/repos/products_repo/products_repo_implementation.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:ecommerce_clean_architecture/core/entities/product_entity.dart';
+import 'package:ecommerce_clean_architecture/core/errors/failures.dart';
+import 'package:ecommerce_clean_architecture/core/models/product_model.dart';
+import 'package:ecommerce_clean_architecture/core/repos/products_repo/products_repo.dart';
+import 'package:ecommerce_clean_architecture/core/services/database_service.dart';
+import 'package:ecommerce_clean_architecture/core/utils/backend_end_points.dart';
+
+class ProductsRepoImplementation implements ProductsRepo {
+  final DatabaseService databaseService;
+  ProductsRepoImplementation({required this.databaseService});
+  @override
+  Future<Either<Failure, List<ProductEntity>>> getProducts() async {
+    try {
+      var data = await databaseService.getData(
+        path: BackendEndPoints.getProducts,
+      );
+      List<ProductEntity> productsList = [];
+      for (var productEntity in data) {
+        productsList.add(ProductModel.fromJson(productEntity).toEntity());
+      }
+      return Right(productsList);
+    } catch (e) {
+      return Left(ServerFailure(message: "فشل إرجاع البيانات"));
+    }
+  }
+}
