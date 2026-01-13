f7d51e8ff6ee378d1218c02c81c0c1ec695f8ae4
Abdallah Mohamed <115114629+AMOHAME0@users.noreply.github.com>
2026-01-13 20:47:31 +0200

    finish forget password ui

--- lib/features/auth/presentation/views/forget_password_view.dart
+++ lib/features/auth/presentation/views/forget_password_view.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_clean_architecture/core/utils/app_styles.dart';
 import 'package:ecommerce_clean_architecture/features/auth/presentation/views/widgets/forget_password_view_body.dart';
 import 'package:flutter/material.dart';
 
@@ -6,6 +7,28 @@ class ForgetPasswordView extends StatelessWidget {
   static const String routeName = "ForgetPassword";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: ForgetPasswordViewBody()));
+    return Scaffold(
+      appBar: buildForgetPasswordAppBar(context),
+      body: SafeArea(child: ForgetPasswordViewBody()),
+    );
+  }
+
+  AppBar buildForgetPasswordAppBar(BuildContext context) {
+    return AppBar(
+      leading: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pop();
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            border: Border.all(color: Color(0xffF1F1F5), width: 1),
+          ),
+          child: Icon(Icons.arrow_back_ios_new),
+        ),
+      ),
+      centerTitle: true,
+      title: Text("نسيان كلمة المرور", style: AppStyles.textStyle19Bold),
+    );
   }
 }
--- lib/features/auth/presentation/views/widgets/forget_password_view_body.dart
+++ lib/features/auth/presentation/views/widgets/forget_password_view_body.dart
@@ -1,3 +1,7 @@
+import 'package:ecommerce_clean_architecture/constants.dart';
+import 'package:ecommerce_clean_architecture/core/utils/app_styles.dart';
+import 'package:ecommerce_clean_architecture/core/widgets/custom_button.dart';
+import 'package:ecommerce_clean_architecture/core/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 
 class ForgetPasswordViewBody extends StatelessWidget {
@@ -5,6 +9,30 @@ class ForgetPasswordViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [Text("ForgetPasswordView")]);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+      child: Column(
+        children: [
+          const SizedBox(height: 24),
+          Text(
+            "لا تقلق ، ما عليك سوى كتابة رقم هاتفك وسنرسل رمز التحقق.",
+            style: AppStyles.textStyle16SemiBold.copyWith(
+              color: Color(0xff616A6B),
+            ),
+          ),
+          const SizedBox(height: 30),
+          CustomTextFormField(
+            hintText: "+20 123 567 8911",
+            onSaved: (value) {},
+          ),
+          const SizedBox(height: 30),
+          SizedBox(
+            height: 54,
+            width: double.infinity,
+            child: CustomButton(text: "نسيت كلمة المرور", onPressed: () {}),
+          ),
+        ],
+      ),
+    );
   }
 }
