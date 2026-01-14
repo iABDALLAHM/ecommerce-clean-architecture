ee2b06eca8aa2f97a159586f14febf11387c6e86
Abdallah Mohamed <115114629+AMOHAME0@users.noreply.github.com>
2026-01-14 10:19:09 +0200

    add image picker field

--- lib/features/home/presentation/views/widgets/profile_information_section.dart
+++ lib/features/home/presentation/views/widgets/profile_information_section.dart
@@ -1,16 +1,33 @@
+import 'dart:io';
+
 import 'package:ecommerce_clean_architecture/core/functions/get_user_data.dart';
 import 'package:ecommerce_clean_architecture/core/utils/app_styles.dart';
+import 'package:ecommerce_clean_architecture/core/widgets/custom_button.dart';
+import 'package:ecommerce_clean_architecture/core/widgets/image_field.dart';
 import 'package:ecommerce_clean_architecture/features/home/presentation/views/widgets/custom_profile_image.dart';
 import 'package:flutter/material.dart';
 
-class ProfileInformationSection extends StatelessWidget {
+class ProfileInformationSection extends StatefulWidget {
   const ProfileInformationSection({super.key, required this.imageUrl});
   final String imageUrl;
+
+  @override
+  State<ProfileInformationSection> createState() =>
+      _ProfileInformationSectionState();
+}
+
+class _ProfileInformationSectionState extends State<ProfileInformationSection> {
+  File? imageFile;
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        CustomProfileImage(imageUrl: imageUrl),
+        GestureDetector(
+          onTap: () {
+            showEditImageBottomSheet(context: context);
+          },
+          child: CustomProfileImage(imageUrl: widget.imageUrl),
+        ),
         const SizedBox(width: 24),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -26,6 +43,39 @@ class ProfileInformationSection extends StatelessWidget {
           ],
         ),
       ],
+    );
+  }
+
+  PersistentBottomSheetController showEditImageBottomSheet({
+    required BuildContext context,
+  }) {
+    return showBottomSheet(
+      context: context,
+      builder: (context) => Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+        decoration: BoxDecoration(color: Colors.white),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ImageField(
+                onChange: (value) {
+                  if (imageFile != null) {
+                    imageFile = value;
+                  } else {
+                    Navigator.pop(context);
+                  }
+                },
+              ),
+              const SizedBox(height: 50),
+              SizedBox(
+                height: 54,
+                width: MediaQuery.sizeOf(context).width * .6,
+                child: CustomButton(text: "تعديل الصورة", onPressed: () {}),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
