b9b1fb8c68b0d7af49c9dfa2f50c45e7d97706dc
Abdallah Mohamed <115114629+AMOHAME0@users.noreply.github.com>
2026-01-14 09:25:31 +0200

    display image in home app bar

--- lib/features/home/presentation/views/widgets/custom_home_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_home_app_bar.dart
@@ -1,9 +1,8 @@
 import 'package:ecommerce_clean_architecture/core/functions/get_user_data.dart';
+import 'package:ecommerce_clean_architecture/core/utils/app_colors.dart';
 import 'package:ecommerce_clean_architecture/core/utils/app_styles.dart';
-import 'package:ecommerce_clean_architecture/core/utils/assets.dart';
 import 'package:ecommerce_clean_architecture/features/home/presentation/views/widgets/custom_notification_icon.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class CustomHomeAppBar extends StatelessWidget {
   const CustomHomeAppBar({super.key});
@@ -12,10 +11,16 @@ class CustomHomeAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 10),
-          child: SvgPicture.asset(Assets.imagesProfilePhoto),
+        CircleAvatar(
+          radius: 25,
+          backgroundColor: AppColors.lightPrimaryColor,
+          child: CircleAvatar(
+            radius: 20,
+            backgroundColor: Colors.transparent,
+            child: Image.network(getUserData().userImage),
+          ),
         ),
+        const SizedBox(width: 11),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
