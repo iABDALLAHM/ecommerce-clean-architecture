93ff0c2416a5e30afe50b4e8e5b2132f6e23980a
Abdallah Mohamed <115114629+AMOHAME0@users.noreply.github.com>
2026-01-18 20:43:02 +0200

    provide get notification cubit

--- lib/features/home/presentation/views/widgets/multi_main_view_bloc_provider.dart
+++ lib/features/home/presentation/views/widgets/multi_main_view_bloc_provider.dart
@@ -1,6 +1,8 @@
 import 'package:ecommerce_clean_architecture/core/cubits/add_favorite_product_cubit/add_favorite_product_cubit.dart';
+import 'package:ecommerce_clean_architecture/core/cubits/get_notifications_cubit/get_notifications_cubit.dart';
 import 'package:ecommerce_clean_architecture/core/cubits/update_user_image_cubit/update_user_image_cubit.dart';
 import 'package:ecommerce_clean_architecture/core/repos/images_repo/images_repo.dart';
+import 'package:ecommerce_clean_architecture/core/repos/notification_repo/notification_repo.dart';
 import 'package:ecommerce_clean_architecture/core/repos/products_repo/products_repo.dart';
 import 'package:ecommerce_clean_architecture/core/services/get_it_service.dart';
 import 'package:ecommerce_clean_architecture/features/home/presentation/manager/cart_cubit/cart_cubit.dart';
@@ -16,8 +18,14 @@ class MultiMainViewBlocProvider extends StatelessWidget {
       providers: [
         BlocProvider(create: (context) => CartCubit()),
         BlocProvider(
-      create: (context) =>
-          UpdateUserImageCubit(imagesRepo: getIt.get<ImagesRepo>()),),
+          create: (context) => GetNotificationsCubit(
+            notificationRepo: getIt.get<NotificationRepo>(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) =>
+              UpdateUserImageCubit(imagesRepo: getIt.get<ImagesRepo>()),
+        ),
         BlocProvider(
           create: (context) =>
               AddFavoriteProductCubit(productsRepo: getIt.get<ProductsRepo>()),
--- lib/features/home/presentation/views/widgets/notification_body.dart
+++ lib/features/home/presentation/views/widgets/notification_body.dart
@@ -1,13 +1,27 @@
 import 'package:ecommerce_clean_architecture/constants.dart';
+import 'package:ecommerce_clean_architecture/core/cubits/get_notifications_cubit/get_notifications_cubit.dart';
 import 'package:ecommerce_clean_architecture/features/home/presentation/views/widgets/custom_notification_app_bar.dart';
 import 'package:ecommerce_clean_architecture/features/home/presentation/views/widgets/in_active_notification_item.dart';
 import 'package:ecommerce_clean_architecture/features/home/presentation/views/widgets/notification_body_header.dart';
 import 'package:ecommerce_clean_architecture/features/home/presentation/views/widgets/active_notification_item.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class NotificationBody extends StatelessWidget {
+class NotificationBody extends StatefulWidget {
   const NotificationBody({super.key});
   static const String routeName = "NotificationView";
+
+  @override
+  State<NotificationBody> createState() => _NotificationBodyState();
+}
+
+class _NotificationBodyState extends State<NotificationBody> {
+  @override
+  void initState() {
+    context.read<GetNotificationsCubit>().getNotification();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
