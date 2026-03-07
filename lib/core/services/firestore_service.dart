55793f3abc1807a93bcc009e929a0534b53cdc2a
Abdallah Mohamed <115114629+AMOHAME0@users.noreply.github.com>
2026-03-07 15:02:34 +0200

    create error handling in firestore

--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommerce_clean_architecture/core/errors/custom_exception.dart';
 import 'package:ecommerce_clean_architecture/core/models/query_prams.dart';
 import 'package:ecommerce_clean_architecture/core/services/database_service.dart';
 
@@ -10,13 +11,25 @@ class FirestoreService implements DatabaseService {
     required String path,
     required Map<String, dynamic> data,
   }) async {
-    await firestore.collection(path).add(data);
+    try {
+      await firestore.collection(path).add(data);
+    } catch (e) {
+      throw CustomException(
+        exceptionMeassge: "حدث خطأ أثناء إضافة البيانات، حاول مرة أخرى",
+      );
+    }
   }
 
   @override
   Future<dynamic> getData({required String path}) async {
-    var data = await firestore.collection(path).get();
-    return data.docs.map((doc) => doc.data()).toList();
+    try {
+      var data = await firestore.collection(path).get();
+      return data.docs.map((doc) => doc.data()).toList();
+    } catch (e) {
+      throw CustomException(
+        exceptionMeassge: "تعذر تحميل البيانات، تحقق من الاتصال بالإنترنت",
+      );
+    }
   }
 
   @override
@@ -25,7 +38,13 @@ class FirestoreService implements DatabaseService {
     required String documentId,
     required Map<String, dynamic> data,
   }) async {
-    await firestore.collection(path).doc(documentId).set(data);
+    try {
+      await firestore.collection(path).doc(documentId).set(data);
+    } catch (e) {
+      throw CustomException(
+        exceptionMeassge: "حدث خطأ أثناء إضافة البيانات، حاول مرة أخرى",
+      );
+    }
   }
 
   @override
@@ -35,12 +54,18 @@ class FirestoreService implements DatabaseService {
     required String documentId,
     required Map<String, dynamic> data,
   }) async {
-    await firestore
-        .collection(path)
-        .doc(documentId)
-        .collection(subCollection)
-        .doc(data["code"])
-        .set(data);
+    try {
+      await firestore
+          .collection(path)
+          .doc(documentId)
+          .collection(subCollection)
+          .doc(data["code"])
+          .set(data);
+    } catch (e) {
+      throw CustomException(
+        exceptionMeassge: "حدث خطأ أثناء إضافة البيانات، حاول مرة أخرى",
+      );
+    }
   }
 
   @override
@@ -48,8 +73,14 @@ class FirestoreService implements DatabaseService {
     required String path,
     required String documentId,
   }) async {
-    var data = await firestore.collection(path).doc(documentId).get();
-    return data.data();
+    try {
+      var data = await firestore.collection(path).doc(documentId).get();
+      return data.data();
+    } catch (e) {
+      throw CustomException(
+        exceptionMeassge: "تعذر العثور على البيانات المطلوبة",
+      );
+    }
   }
 
   @override
@@ -58,12 +89,16 @@ class FirestoreService implements DatabaseService {
     required String subCollection,
     required String documentId,
   }) async {
-    var data = await firestore
-        .collection(path)
-        .doc(documentId)
-        .collection(subCollection)
-        .get();
-    return data.docs.map((doc) => doc.data()).toList();
+    try {
+      var data = await firestore
+          .collection(path)
+          .doc(documentId)
+          .collection(subCollection)
+          .get();
+      return data.docs.map((doc) => doc.data()).toList();
+    } catch (e) {
+      throw CustomException(exceptionMeassge: "تعذر تحميل البيانات المرتبطة");
+    }
   }
 
   @override
@@ -71,27 +106,33 @@ class FirestoreService implements DatabaseService {
     required String path,
     required QueryParams query,
   }) async {
-    Query<Map<String, dynamic>> data = firestore.collection(path);
+    try {
+      Query<Map<String, dynamic>> data = firestore.collection(path);
 
-    for (var condition in query.conditions) {
-      if (condition.isEqualTo != null) {
-        data = data.where(condition.field, isEqualTo: condition.isEqualTo);
+      for (var condition in query.conditions) {
+        if (condition.isEqualTo != null) {
+          data = data.where(condition.field, isEqualTo: condition.isEqualTo);
+        }
+        if (condition.whereIn != null) {
+          data = data.where(condition.field, whereIn: condition.whereIn);
+        }
+        if (condition.arrayContains != null) {
+          data = data.where(
+            condition.field,
+            arrayContains: condition.arrayContains,
+          );
+        }
       }
-      if (condition.whereIn != null) {
-        data = data.where(condition.field, whereIn: condition.whereIn);
-      }
-      if (condition.arrayContains != null) {
-        data = data.where(
-          condition.field,
-          arrayContains: condition.arrayContains,
-        );
-      }
-    }
 
-    for (var condition in query.orders) {
-      data = data.orderBy(condition.field, descending: condition.descending);
+      for (var condition in query.orders) {
+        data = data.orderBy(condition.field, descending: condition.descending);
+      }
+      var result = data.get();
+      return result;
+    } catch (e) {
+      throw CustomException(
+        exceptionMeassge: "حدث خطأ أثناء البحث عن البيانات",
+      );
     }
-    var result = data.get();
-    return result;
   }
 }
